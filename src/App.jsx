a6f3b9cb92613837725f312530d2fa7593955099
droppedyourdingo <huynhdt94@gmail.com>
2023-06-13 13:42:17 -0400

    removed counter, added puppyList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { puppyList } from './data'
+import { useState } from 'react'
 
-
+function App() {
+const [puppies, setPuppies] = useState(puppyList);
+console.log("puppyList: ", puppies);
+  return (
+    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {
+          puppies.map((puppy) => {
+
+            return <p key={puppy.id}>{puppy.name}</p>
+            
+          })
+        }
       </div>
+    </>
+  )
+};
 
 export default App
